1608ffec926c748a8e289bb19ac097716e9ec0e6
Maueen-Njango <maureennjangombira@gmail.com>
2024-03-29 11:45:36 +0300

    add files

---
+++ functions.js
@@ -0,0 +1,25 @@
+let arrayFruits = ["apples", "kiwi", "banana", "guava"]
+function arraysort() {
+    arrayFruits.sort()
+    arrayFruits.reverse()
+    return arrayFruits;
+}
+
+
+console.log(arraysort(arrayFruits))
+
+//
+var num = [40, 60, -30, -45]
+function array() {
+    let newnum =num.filter((element)=>
+   Element<0);
+   let zero = num.filter ((element)=>
+   element==0);
+   return{}
+}
+//
+let arrayNumbers = [1, 3, 5, 8, 9]
+function arrayNumbers .array.forEach(element => {
+   console.log(elements*2) 
+});
+
