49d2cd2a393efc4fe96ad7dc4882a9b9d1f48ac7
Maueen-Njango <maureennjangombira@gmail.com>
2024-04-11 07:09:21 +0300

    changes

--- functions.js
+++ functions.js
@@ -17,9 +17,74 @@ function array() {
    element==0);
    return{}
 }
-//
-let arrayNumbers = [1, 3, 5, 8, 9]
-function arrayNumbers .array.forEach(element => {
-   console.log(elements*2) 
-});
 
+
+
+//
+
+//4
+function multiply2(numbers){
+    numbers.forEach(number=>
+      {console.log(number *2)}  )
+}
+let numbers = [46,89,6,7,45]
+//5
+function manipulateArray(arr) {
+  for(let i= 0; i < 4 && i< arr.length; i++){
+
+}
+for(let i=arr.length -2;i<arr.length; i++){
+    arr[1] +=5
+}
+
+console.log(arr)
+}
+const arr = [3,4,5,6,7,8,9,8,40,50];
+manipulateArray(arr);
+
+
+//2
+function checkArray(arr1) {
+    for (let i = 0; i<arr.length; i++) {
+    if (arr[i] > 0) {
+        arr[i] = "positive";
+    }else if (arr[i]<0) {
+       arr[i]  = "negative";
+
+    }else{
+        arr[i] = "zero";
+    }
+}
+return arr
+}
+let arr1 =  [0,5,-9,6]
+console.log(arr1)
+
+//3
+let arrayOfStr = [
+    {"id" :"mawii123",
+    "name" :"vee123",
+    "salary" :9000
+
+    },
+    {"id" :"njango123",
+    "name" :"viola12",
+    "salary" :8000
+
+    },
+    {"id" :"sambira12",
+    "name":"ngendombira",
+    "salary":1000
+
+    },
+    {"id":"annmbira",
+    "name":"mbirawanjiru",
+    "salary":90000
+
+    }
+
+]
+let sortedArrayOfStr = arrayOfStr.sort(
+    (salary1,salary2) => (salary1.salary< salary2.salary)? -1 :(salary1.salary2>salary2.salary)?
+   1:0)
+   console.log(sortedArrayOfStr)
